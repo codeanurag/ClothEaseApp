5a9bbe2a8052968a6dd4e5c1b08c4137636305d3
Anurag Pandit <codeanuragpandit@gmail.com>
2025-04-01 21:14:34 +0530

    Initial Commit

---
+++ ClothEaseApp/ClothEaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothEaseAppApp.swift
+//  ClothEaseApp
+//
+//  Created by Anurag Pandit on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ClothEaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClothEaseApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClothEaseApp
+//
+//  Created by Anurag Pandit on 01/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
